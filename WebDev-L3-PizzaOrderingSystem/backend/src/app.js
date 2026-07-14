8681cc7327fccf87e8cfc88586da1a62648f0f6d
Victus44 <rennup4288@gmail.com>
2026-07-15 00:46:49 +0530

    Created addInventory API

--- WebDev-L3-PizzaOrderingSystem/backend/src/app.js
+++ WebDev-L3-PizzaOrderingSystem/backend/src/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const authRoutes = require("./routes/auth.routes");
+const inventoryRoutes = require("./routes/inventory.routes");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/auth", authRoutes);
+app.use("/api/inventory", inventoryRoutes);
 
 module.exports = app;
--- WebDev-L3-PizzaOrderingSystem/backend/src/routes/auth.routes.js
+++ WebDev-L3-PizzaOrderingSystem/backend/src/routes/auth.routes.js
@@ -10,10 +10,11 @@ const {
   inventory,
   forgotPassword,
   resetPassword,
-} = require("../controllers/authController");
+} = require("../controllers/auth.controller");
 
 router.post("/register", registerUser);
 router.post("/login", userLogin);
 router.get("/profile", authMiddleware, userProfile);
-router.get("/inventory", authMiddleware, authorize("admin", "user"), inventory);
+router.get("/inventory", authMiddleware, authorize("admin", "user"), inventory); // user allowed as of now to test addInventory, to be
+//  changed after creating an admin account
 module.exports = router;
