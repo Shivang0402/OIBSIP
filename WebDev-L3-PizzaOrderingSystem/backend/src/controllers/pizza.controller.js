e5719e9c1d36c6f967774f0132c6eced7dfc5a5b
Victus44 <rennup4288@gmail.com>
2026-07-15 21:20:46 +0530

    Created an api for updating pizza details

--- WebDev-L3-PizzaOrderingSystem/backend/src/controllers/pizza.controller.js
+++ WebDev-L3-PizzaOrderingSystem/backend/src/controllers/pizza.controller.js
@@ -39,4 +39,46 @@ const getPizza = async (req, res) => {
     });
   }
 };
-module.exports = { addPizza, getPizza };
+
+const updatePizza = async (req, res) => {
+  const { name, description, price, image, isAvailable } = req.body;
+  const { id } = req.params;
+  try {
+    const pizza = await Pizza.findById(id);
+    if (!pizza) {
+      return res.status(404).json({
+        success: false,
+        message: "Pizza does not exist",
+      });
+    }
+
+    if (name !== undefined) {
+      pizza.name = name;
+    }
+    if (description !== undefined) {
+      pizza.description = description;
+    }
+    if (price !== undefined) {
+      pizza.price = price;
+    }
+    if (image !== undefined) {
+      pizza.image = image;
+    }
+    if (isAvailable !== undefined) {
+      pizza.isAvailable = isAvailable;
+    }
+    await pizza.save();
+
+    return res.status(201).json({
+      message: "Pizza details updated successfully.",
+      pizza,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+module.exports = { addPizza, getPizza, updatePizza };
--- WebDev-L3-PizzaOrderingSystem/backend/src/routes/pizza.routes.js
+++ WebDev-L3-PizzaOrderingSystem/backend/src/routes/pizza.routes.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const { addPizza, getPizza } = require("../controllers/pizza.controller");
+const {
+  addPizza,
+  getPizza,
+  updatePizza,
+} = require("../controllers/pizza.controller");
 const { authMiddleware, authorize } = require("../middlewares/authMiddleware");
 
 router.post("/addPizza", authMiddleware, authorize("user", "admin"), addPizza);
 router.get("/getPizza", authMiddleware, authorize("user", "admin"), getPizza);
+router.patch(
+  "/updatePizza/:id",
+  authMiddleware,
+  authorize("admin", "user"),
+  updatePizza,
+);
 
 module.exports = router;
