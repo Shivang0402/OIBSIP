eafad37627d9a9d65bd4da24f78d4e1192b1d1c9
Victus44 <rennup4288@gmail.com>
2026-07-17 17:33:03 +0530

    Made getOrders display result as per the role

--- WebDev-L3-PizzaOrderingSystem/backend/src/controllers/order.controller.js
+++ WebDev-L3-PizzaOrderingSystem/backend/src/controllers/order.controller.js
@@ -68,13 +68,26 @@ const placeOrder = async (req, res) => {
 
 const getOrders = async (req, res) => {
   try {
-    const orders = await Order.find({
-      user: req.user.id,
-    });
-    if (orders.length == 0) {
-      return res.status(404).json({
-        message: "You do not have any orders yet.",
+    let order;
+
+    if (req.user.role === "admin") {
+      orders = await Order.find();
+
+      if (orders.length == 0) {
+        return res.status(404).json({
+          message: "No current orders.",
+        });
+      }
+    } else {
+      orders = await Order.find({
+        user: req.user.id,
       });
+
+      if (orders.length == 0) {
+        return res.status(404).json({
+          message: "You do not have any orders.",
+        });
+      }
     }
     return res.status(201).json({
       orders,
@@ -110,27 +123,9 @@ const getOrderById = async (req, res) => {
     });
   }
 };
-const getAllOrders = async (req, res) => {
-  try {
-    const orders = await Order.find();
-    if (orders.length == 0) {
-      return res.status(404).json({
-        message: "No current orders.",
-      });
-    }
-    return res.status(201).json({
-      orders,
-    });
-  } catch (error) {
-    return res.status(500).json({
-      message: error.message,
-    });
-  }
-};
 
 module.exports = {
   placeOrder,
   getOrders,
   getOrderById,
-  getAllOrders,
 };
--- WebDev-L3-PizzaOrderingSystem/backend/src/routes/order.routes.js
+++ WebDev-L3-PizzaOrderingSystem/backend/src/routes/order.routes.js
@@ -5,20 +5,16 @@ const {
   placeOrder,
   getOrders,
   getOrderById,
-  getAllOrders,
 } = require("../controllers/order.controller");
 const { authMiddleware, authorize } = require("../middlewares/authMiddleware");
 
 router.post("/placeOrder", authMiddleware, authorize("user"), placeOrder); //user
-router.get("/getOrders", authMiddleware, authorize("user"), getOrders); //user
+router.get("/getOrders", authMiddleware, authorize("user", "admin"), getOrders);
 router.get(
   "/getOrders/:id",
   authMiddleware,
   authorize("user", "admin"),
   getOrderById,
-); //user
-
-// router.get("/getOrders/:id", authMiddleware, authorize("admin"), getOrderById); //admin
-router.get("/getAllOrders", authMiddleware, authorize("admin"), getAllOrders); //admin
+);
 
 module.exports = router;
