1ee9f99ea3acabe76d9f45e6b9981b56929df3eb
Victus44 <rennup4288@gmail.com>
2026-07-24 14:24:47 +0530

    Configured socket.io to server

--- WebDev-L3-PizzaOrderingSystem/backend/src/server.js
+++ WebDev-L3-PizzaOrderingSystem/backend/src/server.js
@@ -1,16 +1,30 @@
 require("dotenv").config();
-
+const http = require("http");
+const { Server } = require("socket.io");
 const app = require("./app");
 const connectDb = require("./config/db");
-
 const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
   await connectDb();
 
-  app.listen(PORT, () => {
+  const server = http.createServer(app);
+
+  const io = new Server(server, {
+    cors: {
+      origin: "*",
+    },
+  });
+
+  io.on("connection", (socket) => {
+    console.log(`User connected: ${socket.id}`);
+  });
+  io.on("disconnect", () => {
+    consolelog(`User disconnected: ${socket.id}`);
+  });
+
+  server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
 };
-
 startServer();
