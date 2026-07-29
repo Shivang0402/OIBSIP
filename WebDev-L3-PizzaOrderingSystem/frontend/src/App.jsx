521525ed73ab14b8f138f7c54bdc95314a6e866b
Victus44 <rennup4288@gmail.com>
2026-07-29 18:15:43 +0530

    Created register page UI

--- WebDev-L3-PizzaOrderingSystem/frontend/src/App.jsx
+++ WebDev-L3-PizzaOrderingSystem/frontend/src/App.jsx
@@ -1,7 +1,8 @@
+import './App.css';
+import Register from './pages/Register';
+
 function App() {
-  return (
-    <h1>PizzaExpress</h1>
-  );
+  return <Register />;
 }
 
 export default App;
---
+++ WebDev-L3-PizzaOrderingSystem/frontend/src/pages/Register.jsx
@@ -0,0 +1,21 @@
+import '../styles/Register.css';
+
+function Register() {
+  return (
+    <div className="register-screen">
+      <div className="register-card">
+        <h1>Register</h1>
+        <form className="register-form">
+          <input type="text" placeholder="Name" required />
+          <input type="email" placeholder="Email" required />
+          <input type="password" placeholder="Password" required />
+          <input type="password" placeholder="Confirm pass" required />
+          <input type="tel" placeholder="Phone" required />
+          <button type="submit">Register</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Register;
