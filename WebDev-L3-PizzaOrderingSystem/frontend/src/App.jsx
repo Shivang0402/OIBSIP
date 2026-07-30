622cb8392612f23933f0431487acd61dbba8bde4
Victus44 <rennup4288@gmail.com>
2026-07-30 16:38:12 +0530

    Implememted react-dom-routes for login and register

--- WebDev-L3-PizzaOrderingSystem/frontend/src/App.jsx
+++ WebDev-L3-PizzaOrderingSystem/frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css';
-import Register from './pages/Register';
+import { Routes, Route } from 'react-router-dom';
+import Register from './pages/register';
 import Login from './pages/login';  
 
 function App() {
-  // return <Register />;
-  return <Login />
+  return(
+<Routes>
+  <Route path="/register" element={<Register />}/>
+  <Route path="/login" element={<Login />}/>
+</Routes>
+);
 }
 
 export default App;
